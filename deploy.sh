8c603065394c658f7a508c603d5c06c539394b90
AlexCerlat <114800239+AlexCerlat@users.noreply.github.com>
2025-07-21 09:40:41 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@
 
 echo
 echo File html was rsync with remote nginx file
-sudo rsync -avc /tmp/myscripts/html/html ubuntu@18.199.146.184:/var/www/html/index.html
+sudo rsync -avc /tmp/myscripts/html/html ubuntu@public_ip:/var/www/html/index.html
 echho
 echo Check your page after 5 min you need to se new website
